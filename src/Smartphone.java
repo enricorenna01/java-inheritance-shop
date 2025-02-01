0285a2555bf7513f6deb77e5824b0a4b1d18c7b4
enricorenna01 <erenna39@gmail.com>
2025-02-01 22:58:56 +0100

    feat: added Smartphone class

---
+++ src/Smartphone.java
@@ -0,0 +1,42 @@
+public class Smartphone extends Prodotto {
+    private String imei;
+    private int memoria;
+
+    // Costruttore
+    public Smartphone(int codice, String nome, String marca, double prezzo, double iva, String imei, int memoria) {
+        super(codice, nome, marca, prezzo, iva);
+        this.imei = imei;
+        this.memoria = memoria;
+    }
+
+    // Getter e Setter
+    public String getImei() {
+        return imei;
+    }
+
+    public void setImei(String imei) {
+        this.imei = imei;
+    }
+
+    public int getMemoria() {
+        return memoria;
+    }
+
+    public void setMemoria(int memoria) {
+            this.memoria = memoria;
+    }
+
+    //Metodi
+    public double getPrezzoScontato(boolean haTessera) {
+        double prezzoScontato = super.getPrezzoScontato(haTessera);
+    
+        if (memoria < 32) {
+            prezzoScontato *= 0.95; // Sconto del 5%
+        } else {
+            prezzoScontato *= 0.98; // Sconto del 2%
+        }
+
+        return prezzoScontato;
+    }
+
+}
